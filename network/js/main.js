a8f4ad1e46cfca360b9bcc1bf1d3c50a47ccc0a4
arcewander <wander.arcedeveloper@gmail.com>
2021-12-12 20:44:10 -0400

    cor do hover no mapa

--- network/js/main.js
+++ network/js/main.js
@@ -208,7 +208,7 @@ function configSigmaElements(config) {
     // Node hover behaviour
     if (config.features.hoverBehavior == "dim") {
 
-        var greyColor = '#ccc';
+        var greyColor = '#333';
         sigInst.bind('overnodes', function(event) {
             var nodes = event.content;
             var neighbors = {};
